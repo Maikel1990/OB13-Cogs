1c612e172b58e3e8ffe0658dd0933aa3d8ab5e2a
azcraze <15200975+azcraze@users.noreply.github.com>
2023-05-11 03:48:59 -0500

    Update __init__.py
    added missing await

--- translate/__init__.py
+++ translate/__init__.py
@@ -2,9 +2,8 @@ import json
 from pathlib import Path
 from .translate import Translate
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
 async def setup(bot):
-    bot.add_cog(Translate(bot))
+    with open(Path(__file__).parent / "info.json") as fp:
+        __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+    
+    await bot.add_cog(Translate(bot))
